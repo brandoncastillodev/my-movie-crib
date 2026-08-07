ae0a74f432c4fe075bac5b169bd6874b61268be3
brandoncastillodev <brandoncastillo.09@gmail.com>
2026-08-06 21:58:43 -0300

    fix: prevent CLS with aspect-ratio and lazy loading

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -47,6 +47,7 @@ function Home() {
                     <img
                       src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                       alt={movie.title}
+                      loading="lazy"
                     />
                   </Link>
                 </div>
